c9de0a44c00078d65a89c968fbe3baa870448bd7
Angel G300 Saucedo <saucedog926@gmail.com>
2024-04-24 20:34:07 -0300

    discord bot

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-##imports
 import discord
-from discord import app_commands
+# from discord import app_commands
 from discord.ext import commands
-import os
-from dotenv import load_dotenv
 
-### cargamos variables de entorno
-load_dotenv()
 ##estructura de control
 BOT_PREFIX = "!!"
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-
-# class MyClient(discord.Client):
-#     async def on_ready(self):
-#         print(f'Logged on as {self.user}!')
-
-#     async def on_message(self, message):
-#         print(f'Message from {message.author}: {message.content}')
-
-# client = MyClient(intents=intents)
-# client.run(DISCORD_TOKEN)
+DISCORD_TOKEN = "DISCORD_BOT_SECRET_TOKEN"
 
 ## INTENTS
 intents = discord.Intents.all()
@@ -29,12 +14,12 @@ bot = commands.Bot(command_prefix=BOT_PREFIX,description="hola soy un bot",inten
 
 @bot.event
 async def on_ready():
+    print(f"el bot {bot.user} ya esta prendido")
     await bot.tree.sync()
     print("sincronizado correctamente")
-    print(f"el bot {bot.user} ya esta prendido")
 
-@bot.hybrid_command(name="ping", description = "test")
-async def ping(interaction: discord.Interaction):
-    await interaction.reply(content="pong!") 
+@bot.hybrid_command(name="insignea", description = "slash command")
+async def insignea(interaction: discord.Interaction):
+    await interaction.reply(content="reclamar la insignea de desarrollador en unos dias:\n https://discord.com/developers/active-developer")
 
 bot.run(DISCORD_TOKEN)
